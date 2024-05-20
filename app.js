727f2950457fd4582c6e6d640516f52fb558fde0
Nathaniel Roberts <nathaniel.roberts@students.mq.edu.au>
2024-05-20 17:14:05 +1000

    Hugo Setup

--- app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add an event listener to the button
-    document.getElementById('reveal-button').addEventListener('click', function() {
-        // Change the content of the paragraph when the button is clicked
-        document.getElementById('dynamic-content').innerText = 'You found the secret message! 👏';
-    });
-});
